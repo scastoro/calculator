e54846ecb6cff50c381944f68803d50091c22805
Sal Castoro <salcastoro@gmail.com>
2022-01-03 16:55:09 -0500

    Add intial logic for number and operator buttons

--- script.js
+++ script.js
@@ -27,3 +27,44 @@ function operate(operator, num1, num2){
     return multiply(num1, num2);
   }
 }
+const display = document.querySelector('.display');
+const clearBtn = document.querySelector('.clear')
+let firstNumber;
+let secondNumber;
+let operatorValue;
+
+// Clear display div when clear button is pressed
+function clearDisplay(){
+  display.innerText = '';
+}
+
+clearBtn.addEventListener('click', clearDisplay);
+
+// Function to populate display with clicked number
+function populateDisplay(obj){
+  display.innerText += obj.target.innerText
+  console.dir(obj.target.innerText);
+}
+
+const numButtons = document.querySelectorAll('.btn-num');
+numButtons.forEach((btn) => {
+  btn.addEventListener('click', populateDisplay);
+});
+
+// Function to run when operator buttons are pressed
+function selectOperator(obj){
+  if(!firstNumber) {
+    firstNumber = display.innerText;
+  } else {
+    secondNumber = display.innerText;
+    operate(operatorValue, firstNumber, secondNumber);
+  }
+  operatorValue = obj.target.innerText;
+  console.dir(operatorValue);
+  display.innerText = '';
+}
+
+const operatorButtons = document.querySelectorAll('.btn-operator');
+operatorButtons.forEach((btn) => {
+  btn.addEventListener('click', selectOperator);
+});
