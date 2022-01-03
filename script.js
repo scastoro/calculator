cd2c9f37910844df9f67085d295a07cf11ae2821
Sal Castoro <salcastoro@gmail.com>
2022-01-03 10:25:44 -0500

    Add basic math functions and operate function to script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Create the basic math functions
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function divide(num1, num2){
+  return num1 / num2;
+}
+
+// Create operator function
+function operate(operator, num1, num2){
+  if (operator === '+'){
+    return add(num1, num2);
+  } else if (operator === '-') {
+    return subtract(num1, num2);
+  }else if (operator === '/') {
+    return divide(num1, num2);
+  }else if (operator === '*') {
+    return multiply(num1, num2);
+  }
+}
