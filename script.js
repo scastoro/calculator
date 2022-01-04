bd1ce573cb0ba9d8c22a6b34c5aa531be0ce29f8
Sal Castoro <salcastoro@gmail.com>
2022-01-04 11:42:11 -0500

    Add variable checking logic to operate function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const display = document.querySelector('.display');
+const clearBtn = document.querySelector('.clear')
+let firstNumber;
+let secondNumber;
+let operatorValue;
+let isCleared = false;
+
 // Create the basic math functions
 function add(num1, num2){
   return parseFloat(num1) + parseFloat(num2);
@@ -16,7 +23,10 @@ function divide(num1, num2){
 }
 
 // Create operator function
-function operate(operator, num1, num2){
+function operate(operator, num1, num2, clear){
+  if (!operator || !num1 || !num2 || !clear){
+    return 'Error.';
+  }
   if (operator === '+'){
     return add(num1, num2);
   } else if (operator === '-') {
@@ -27,14 +37,8 @@ function operate(operator, num1, num2){
     return multiply(num1, num2);
   }
 }
-const display = document.querySelector('.display');
-const clearBtn = document.querySelector('.clear')
-let firstNumber;
-let secondNumber;
-let operatorValue;
-let isCleared = true;
 
-// Clear display div when clear button is pressed
+// Clear display div and reset global variables when clear button is pressed
 function clearDisplay(){
   display.innerText = '';
   firstNumber = null;
@@ -46,9 +50,10 @@ clearBtn.addEventListener('click', clearDisplay);
 
 // Function to populate display with clicked number
 function populateDisplay(obj){
-  if(isCleared) {
+  if(!isCleared) {
     display.innerText = '';
-    isCleared = false;
+    isCleared = true;
+    console.log(isCleared);
   }
   display.innerText += obj.target.innerText
   console.dir(obj.target.innerText);
@@ -65,22 +70,23 @@ function selectOperator(obj){
     firstNumber = display.innerText;
     operatorValue = obj.target.innerText;
     console.log(firstNumber);
-    isCleared = true;
+    isCleared = false;
   } else {
     secondNumber = display.innerText;
     console.log(secondNumber);
-    let displayOutput = operate(operatorValue, firstNumber, secondNumber);
+    let displayOutput = operate(operatorValue, firstNumber, secondNumber, isCleared);
+    isCleared = false;
     operatorValue = obj.target.innerText;
     console.dir(displayOutput);
     display.innerText = displayOutput;
     firstNumber = displayOutput;
     secondNumber = null;
-    isCleared = true;
+    
   }
   
   
 }
-
+                                                                                        
 const operatorButtons = document.querySelectorAll('.btn-operator');
 operatorButtons.forEach((btn) => {
   btn.addEventListener('click', selectOperator);
@@ -89,8 +95,8 @@ operatorButtons.forEach((btn) => {
 // Run operate function when equals sign is pressed
 function evaluate(){
   secondNumber = display.innerText;
-  display.innerText = operate(operatorValue, firstNumber, secondNumber);
-  isCleared = true;
+  display.innerText = operate(operatorValue, firstNumber, secondNumber, isCleared);
+  isCleared = false;
   firstNumber = null;
   secondNumber = null;
 }
